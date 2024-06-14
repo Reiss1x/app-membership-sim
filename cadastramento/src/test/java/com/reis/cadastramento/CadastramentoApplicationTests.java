db4f79bc246829073f49b516e40345bf24d774df
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-06-14 15:00:25 -0300

    feat: first commit

---
+++ cadastramento/src/test/java/com/reis/cadastramento/CadastramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis.cadastramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
