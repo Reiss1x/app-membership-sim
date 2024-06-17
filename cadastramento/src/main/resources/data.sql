995b15b7c09743cb47d03744c824e0cfcd531f09
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-06-16 22:23:44 -0300

    fix: config files

---
+++ cadastramento/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO cliente (nome, email) VALUES ('Ana Santos', 'ana.santos@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Pedro Lima', 'pedro.lima@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Mariana Costa', 'mariana.costa@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Rafaela Almeida', 'rafaela.almeida@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Fernando Pereira', 'fernando.pereira@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Carla Santos', 'carla.santos@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Antônio Oliveira', 'antonio.oliveira@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Juliana Rodrigues', 'juliana.rodrigues@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Luiz Souza', 'luiz.souza@example.com');
+INSERT INTO cliente (nome, email) VALUES ('Patrícia Alves', 'patricia.alves@example.com');
+
+INSERT INTO aplicativo (nome, custo_mensal) VALUES ('Clash Royale', 5.50);
+INSERT INTO aplicativo (nome, custo_mensal) VALUES ('Netflix', 15.99);
+INSERT INTO aplicativo (nome, custo_mensal) VALUES ('Spotify', 9.99);
+INSERT INTO aplicativo (nome, custo_mensal) VALUES ('Amazon Prime Video', 12.90);
+INSERT INTO aplicativo (nome, custo_mensal) VALUES ('Subway Surfers', 7.99);
+
+INSERT INTO assinatura (cod_app, cod_cli, inicio_vigencia, fim_vigencia)VALUES (1, 1, '2024-06-16', '2024-06-23');
+INSERT INTO assinatura (cod_app, cod_cli, inicio_vigencia, fim_vigencia)VALUES (2, 2, '2024-06-15', '2024-06-22');
+INSERT INTO assinatura (cod_app, cod_cli, inicio_vigencia, fim_vigencia)VALUES (3, 3, '2024-06-14', '2024-06-21');
+INSERT INTO assinatura (cod_app, cod_cli, inicio_vigencia, fim_vigencia)VALUES (4, 4, '2024-06-13', '2024-06-20');
+INSERT INTO assinatura (cod_app, cod_cli, inicio_vigencia, fim_vigencia)VALUES (5, 5, '2024-06-8', '2024-06-15');
