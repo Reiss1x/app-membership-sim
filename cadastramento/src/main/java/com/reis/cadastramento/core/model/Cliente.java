06eec367bb3c2b0b2248364ceda49c0f241f215c
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-06-16 22:21:51 -0300

    fix: config files

--- cadastramento/src/main/java/com/reis/cadastramento/core/model/Cliente.java
+++ cadastramento/src/main/java/com/reis/cadastramento/core/model/Cliente.java
@@ -2,6 +2,8 @@ package com.reis.cadastramento.core.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -13,6 +15,7 @@ import lombok.*;
 @Builder
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cod;
     private String nome;
     private String email;
