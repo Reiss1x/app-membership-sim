b7bb0e7a50d389c6707e2e3b27d55e1d69f53000
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-06-20 11:13:43 -0300

    fix: basic client crud

--- cadastramento/src/main/java/com/reis/cadastramento/core/services/ClienteService.java
+++ cadastramento/src/main/java/com/reis/cadastramento/core/services/ClienteService.java
@@ -1,12 +1,14 @@
 package com.reis.cadastramento.core.services;
 
 import com.reis.cadastramento.core.model.Cliente;
+import com.reis.cadastramento.core.model.Cliente;
 import com.reis.cadastramento.core.model.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -16,5 +18,20 @@ public class ClienteService {
 
     public List<Cliente> getAllCliente(){
         return (List<Cliente>) repo.findAll();
+    }
+
+    public Cliente createCliente(Cliente aplicativo){
+        return repo.save(aplicativo);
+    }
+
+    public Cliente updateCliente(Cliente cliente){
+        Optional<Cliente> tempClient = repo.findById(cliente.getCod());
+        if(tempClient.isPresent()){
+            Cliente client = tempClient.get();
+            client.setNome(cliente.getNome());
+            client.setEmail(cliente.getEmail());
+            return client;
+        }
+        return null;
     }
 }
--- cadastramento/src/main/java/com/reis/cadastramento/presenter/ClienteController.java
+++ cadastramento/src/main/java/com/reis/cadastramento/presenter/ClienteController.java
@@ -1,10 +1,13 @@
 package com.reis.cadastramento.presenter;
 
 import com.reis.cadastramento.core.model.Cliente;
+import com.reis.cadastramento.core.model.Cliente;
 import com.reis.cadastramento.core.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +21,16 @@ public class ClienteController {
     @GetMapping("/servcad/clientes")
     public List<Cliente> getAllClientes(){
         return cs.getAllCliente();
+    }
+
+    @PostMapping("servcad/clientes/")
+    public Cliente postCliente(@RequestBody Cliente aplicativo){
+        return cs.createCliente(aplicativo);
+    }
+
+    @PostMapping("servcad/clientes/update")
+    public Cliente updateCliente(@RequestBody Cliente clienteNovo){
+        return cs.updateCliente(clienteNovo);
     }
 
 }
